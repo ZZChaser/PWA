c248fb9d880ea5c07ac658cd007ce0c0f1e61494
zhuzhao <1348832049@qq.com>
2020-04-17 14:25:41 +0800

    update sw.js

--- src/app/serviceWorker/index.js
+++ src/app/serviceWorker/index.js
@@ -1,6 +1,6 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js');
 
-const cacheStorageKey = "my-pwa";
+const cacheStorageKey = 'my-pwa';
 
 const cacheList = [
   '../../../',
@@ -9,10 +9,38 @@ const cacheList = [
   '../../../src/assets/icons/app_icon.svg',
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(cacheStorageKey)
-      .then(cache => cache.addAll(cacheList))
+    caches
+      .open(cacheStorageKey)
+      .then((cache) => cache.addAll(cacheList))
       .then(() => self.skipWaiting())
-  )
-})
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request.url);
+      }
+    })
+  );
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.filter((cacheName) => cacheName !== cacheStorageKey)
+            .map((cacheName) => caches.delete(cacheName))
+        );
+      })
+      .then(() => {
+        return self.clients.claim();
+      })
+  );
+});
