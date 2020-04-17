b5f6e191ec5bf7b8bd6ae9a73c4cd508aea086ca
zhuzhao <1348832049@qq.com>
2020-04-17 13:58:05 +0800

    modify path

--- src/app/serviceWorker/index.js
+++ src/app/serviceWorker/index.js
@@ -3,10 +3,10 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox
 const cacheStorageKey = "my-pwa";
 
 const cacheList = [
-  '/',
-  'index.html',
-  'src/assets/js/index.js',
-  'src/assets/icons/app_icon.svg',
+  '../../../',
+  '../../../index.html',
+  '../../..//assets/js/index.js',
+  '../../..//assets/icons/app_icon.svg',
 ];
 
 self.addEventListener('install', event => {
